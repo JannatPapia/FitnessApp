9ce9aba26eb4812b0b759ce4240464c0ff5da3d4
Jannatpapia <papiamcode@gmail.com>
2022-03-17 01:36:19 +0600

    Building Fitness Ring Progress View

--- FitnessApp/View/FitnessRingCardView.swift
+++ FitnessApp/View/FitnessRingCardView.swift
@@ -14,12 +14,26 @@ struct FitnessRingCardView: View {
                 .fontWeight(.semibold)
                 .frame(maxWidth: .infinity, alignment: .leading)
             
-            HStack{
+            HStack(spacing: 20){
+                
+                //Progress Ring
+                ZStack{
                 ForEach(rings.indices, id: \.self) { index in
+                    ZStack{
                     Circle()
+                        .stroke(.gray.opacity(0.3), lineWidth: 10)
+                    
+                    Circle()
+                            .trim(from: 0, to: rings[index].progress / 100)
+                        .stroke(rings[index].keyColor,style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                        .rotationEffect(.init(degrees: -90.0))
                 }
+                .padding(CGFloat(index) * 16)
             }
         }
+        .frame(width: 130, height: 130)
+    }
+    }
     }
 }
 
