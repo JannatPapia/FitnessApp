df7ef8701e6ead90cd70065b8fb23f4b1176288d
Jannatpapia <papiamcode@gmail.com>
2022-03-16 21:11:32 +0600

    Project Setup Done

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Jannatun Nahar Papia  on 16/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
